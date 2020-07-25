bacda71405381d9fa7b4d193266f3f9451837483
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-07-26 00:37:41 +0900

    create: 記事投稿機能の実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
 use App\Article;
 
 class ArticleController extends Controller
@@ -12,5 +13,24 @@ class ArticleController extends Controller
         $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    // 記事作成画面
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * ArticleRequestとArticleのインスタンスを作成
+     *
+     */
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }
