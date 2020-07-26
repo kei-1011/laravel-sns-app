2cf4c65809dff14f9def29f674139fb42dbac97f
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-07-26 16:06:13 +0900

    create:記事削除機能の実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -34,10 +34,30 @@ class ArticleController extends Controller
     }
 
     /**
-     * 記事更新
+     * 記事更新画面
      */
     public function edit(Article $article)
     {
         return view('articles.edit', ['article' => $article]);
+    }
+
+    /**
+     * 記事更新
+     */
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    /**
+     * 記事削除
+     */
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
     }
 }
